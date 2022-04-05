b1db3d203c80a1b72d02c8fc2c3b98630e2525ff
Nutifafa Attor <attorfafa@gmail.com>
2022-04-05 09:58:30 +0000

    added instructions

--- Assignment-6/validate_form.js
+++ Assignment-6/validate_form.js
@@ -1,1 +1,13 @@
 // This file validates the HTML form it is linked to
+
+// Create an HTML form similar to the one attached to this assignment and use JavaScript to validate user inputs for the HTML form 
+//         (Do not bother about design). Create a JavaScript file to do the validation and call it validate_form.js file. 
+//         Submit both HTML and JavaScript file.
+
+//         The validation should satisfy the following rules at least:
+
+//             No empty input. All form elements should have an input.
+//             Exception for rule one: If you choose "No" for "Current Medication", the textarea for "Current Medication" must be empty. Otherwise, there should be an input to indicate the details, i.e., if you choose "Yes", the textarea should not be empty.
+//             Maximum input characters of "First name" or "Last name" are 50 characters.
+//             Maximum input characters of "Address" are 300 characters.
+//             For correct user inputs, show the user inputs in an alert window. Otherwise, warn the user in an alert window. 
